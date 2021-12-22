f65f9f96e303254b6efbcf766049e878ff4a14d1
Mustafa-Siddique <66897095+Mustafa-Siddique@users.noreply.github.com>
2021-12-22 17:36:17 +0530

    Updates

---
+++ precache-manifest.fa4251cf6e80bb993aee86852e54b62b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5544d5a627db70f6b4e26577fa7aea73",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "5741f92da3ab8b3365a2",
+    "url": "/textutils/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "93411b8e5f68741c0253",
+    "url": "/textutils/static/js/2.ecc23d82.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/textutils/static/js/2.ecc23d82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acf06f859e3f9fc63a1c",
+    "url": "/textutils/static/js/3.591c53ed.chunk.js"
+  },
+  {
+    "revision": "5741f92da3ab8b3365a2",
+    "url": "/textutils/static/js/main.95111044.chunk.js"
+  },
+  {
+    "revision": "57a44e868182f7d5db87",
+    "url": "/textutils/static/js/runtime-main.c301572e.js"
+  }
+]);
